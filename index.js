bc1d4c1bee1f787d7cd68840f21303a0768f2213
daggggy <deogracias.rey.bonifacio@gmail.com>
2019-06-22 14:16:37 +0700

    created database connection

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express()
 
 const port = 3000
 
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'inventory'
+});
+
 app.listen(port,() => {
     console.log('Listening to port ' + port)
 })
