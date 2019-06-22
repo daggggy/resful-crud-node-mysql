dd8524ff1e9df1a840bcbc24d8a970ef860e1ab1
daggggy <deogracias.rey.bonifacio@gmail.com>
2019-06-22 14:25:09 +0700

    created a route to show specific item

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.get('/inventory', (req, res)=>{
     })    
 });
 
+app.get('/inventory/:id', (req, res)=>{
+    db.query("SELECT * FROM `items` WHERE id = ?",[req.params.id], (err, results) => {
+        if(err) throw(err);
+        console.log(results);
+        res.send(results)
+    })    
+});
+
 app.listen(port,() => {
     console.log('Listening to port ' + port)
 })
